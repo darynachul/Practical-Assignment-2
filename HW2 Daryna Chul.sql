e36f4824d895210c0cb6f131cfcd5f42e486482c
darynachul <dchul@kse.org.ua>
2025-10-21 21:48:54 +0300

    Add files via upload

--- HW2 Daryna Chul.sql
+++ HW2 Daryna Chul.sql
@@ -46,28 +46,30 @@ GROUP BY c.customer_id, c.name, c.email;
 
 
 --  OPTIMIZED QUERY
-CREATE INDEX idx_order_items_order_id ON order_items(order_id); -- Creation of this index on the order_id column helps to speed up JOIN between tables orders and order_items
-CREATE INDEX idx_orders_status ON orders(status); -- Creation of this index on the status column helps to find completed orders fast without scanning all table 
+CREATE INDEX idx_orders_status_customer ON orders(status, customer_id); -- Creation of this index on the status column and customer_id column helps to find completed orders fast without scanning all table 
+CREATE INDEX idx_order_items_cover ON order_items(order_id, quantity, price); -- Creation of this covering index helps to exptract price and quantity for query, speed up JOIN between tables orders and order_items 
+CREATE INDEX idx_customers_grouping ON customers(customer_id, email, name); -- Creation of this index extracts customer_id, email, name required for grouping and helps to avoid scanning all table
 
 WITH completed_orders AS (
     SELECT o.customer_id, o.order_id
-    FROM orders o USE INDEX (idx_orders_status)
+    FROM orders o USE INDEX (idx_orders_status_customer) -- USE INDEX: On the positive side, USE INDEX indicates the correct index, reduces full table scans and helps speed up queries. On the negative side, it can slow down the process if the data changes.
     WHERE o.status = 'Completed' -- Creation CTE completed_orders and filtering columns with the status "completed" and using an index for faster searching   
 )
 SELECT -- It is the main query which selects customer_id, their name and email and counts quantity of good they bought and their total amount for purchase
     c.customer_id, c.name, c.email,
     SUM(oi.quantity) AS total_products,
     SUM(oi.quantity * oi.price) AS total_payment
-FROM customers c 
-JOIN completed_orders co ON c.customer_id = co.customer_id -- Merge table customers with the completed_orders 
-JOIN order_items oi USE INDEX (idx_order_items_order_id) ON co.order_id = oi.order_id -- Merge each completed order with items from order_item using index for faster searching
+FROM customers c USE INDEX (idx_customers_grouping) 
+JOIN completed_orders co USE INDEX (idx_order_items_cover) ON c.customer_id = co.customer_id -- Merge table customers with the completed_orders using index
+JOIN order_items oi USE INDEX (idx_order_items_cover) ON co.order_id = oi.order_id -- Merge each completed order with items from order_item using index
 GROUP BY c.customer_id, c.name, c.email; -- Groups the results based on customer_id, name and email
--- USE INDEX: On the positive side, USE INDEX indicates the correct index, reduces full table scans and helps speed up queries. On the negative side, it can slow down the process if the data changes.
+
 
 
 -- EXECUTION PLANS
 -- Non-optimized query takes longer to execute because it scans three tables with large amounts of data (full-table scan). Moreover, filtration occurs after JOIN, it's ineffectively as time is spent for unnessesary merging of each row from the tables
 -- Optimized query executes faster as it includes indexes(faster scanning), filtration occurs before JOIN in CTE which puts less strain on memory.
+-- EXPLAIN ANALYZE
 --  NON-OPTIMIZED QUERY 
 EXPLAIN ANALYZE
 SELECT 
@@ -85,18 +87,43 @@ GROUP BY c.customer_id, c.name, c.email;
 EXPLAIN ANALYZE
 WITH completed_orders AS (
     SELECT o.customer_id, o.order_id
-    FROM orders o USE INDEX (idx_orders_status)
+    FROM orders o USE INDEX (idx_orders_status_customer)
     WHERE o.status = 'Completed'
 )
 SELECT 
     c.customer_id, c.name, c.email,
     SUM(oi.quantity) AS total_products,
     SUM(oi.quantity * oi.price) AS total_payment
-FROM customers c
-JOIN completed_orders co ON c.customer_id = co.customer_id
-JOIN order_items oi USE INDEX (idx_order_items_order_id) ON co.order_id = oi.order_id
+FROM customers c USE INDEX (idx_customers_grouping)
+JOIN completed_orders co USE INDEX (idx_order_items_cover) ON c.customer_id = co.customer_id
+JOIN order_items oi USE INDEX (idx_order_items_cover) ON co.order_id = oi.order_id
 GROUP BY c.customer_id, c.name, c.email;
 
-
+-- EXPLAIN
+-- NON-OPTIMIZED QUERY 
+EXPLAIN
+SELECT
+  c.name,
+  SUM(oi.quantity * oi.price)
+FROM customers c IGNORE INDEX (idx_customers_grouping)
+JOIN orders o IGNORE INDEX (idx_orders_status_customer) ON c.customer_id = o.customer_id
+JOIN order_items oi  IGNORE INDEX (idx_order_items_cover) ON o.order_id = oi.order_id
+WHERE o.status = 'Completed'
+GROUP BY c.customer_id, c.name, c.email;
+--  OPTIMIZED QUERY
+EXPLAIN 
+WITH completed_orders AS (
+    SELECT o.customer_id, o.order_id
+    FROM orders o USE INDEX (idx_orders_status_customer)
+    WHERE o.status = 'Completed'
+)
+SELECT 
+    c.customer_id, c.name, c.email,
+    SUM(oi.quantity) AS total_products,
+    SUM(oi.quantity * oi.price) AS total_payment
+FROM customers c USE INDEX (idx_customers_grouping)
+JOIN completed_orders co USE INDEX (idx_order_items_cover) ON c.customer_id = co.customer_id
+JOIN order_items oi USE INDEX (idx_order_items_cover) ON co.order_id = oi.order_id
+GROUP BY c.customer_id, c.name, c.email;
 
 
